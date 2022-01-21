4d9b954a9536ab27c7a00816621ed088cfacfbb3
Amir Ghaderi <altghaderi@gmail.com>
2022-01-21 12:10:30 -0700

    added new logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,10 @@
 // import Image from "react-bootstrap/Image";
-const image_src = require("../images/shap_logo.png");
+const image_src = require("../images/shap_logo2.png");
 
 const Logo = () => {
   return (
     <section className="d-flex flex-row justify-content-center mt-4">
-      <img src={image_src} alt="shap logo" height="250px" />
+      <img src={image_src} alt="shap logo" height="200px" />
     </section>
   );
 };
